072f28d5a19924e0af2d4c5881c24697659fcc0c
Kenneth Angelikas <tuq24729@temple.edu>
2023-01-30 10:18:07 -0500

    Greetings, Assignment 1. complete

--- src/com/javawork/labone/Greeetings.java
+++ src/com/javawork/labone/Greeetings.java
@@ -1,7 +1,11 @@
 package JavaClassWork.src.com.javawork.labone;
-
+// Kenneth Angelikas
+// 1/30/22
+// Assignment 1. Greetings
+//
+// This assignment is learning how to set up a working java program and using a println to greet our profesor
 public class Greeetings {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Good morning, Profesor Fiore.");
     }
 }

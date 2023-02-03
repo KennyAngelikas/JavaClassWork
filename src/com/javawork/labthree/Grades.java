c7122cc0ee5210b21d8069d4d9e050a8bd03562b
Kenneth Angelikas <tuq24729@temple.edu>
2023-02-03 10:32:27 -0500

    lab2 song.java complete 1/31/22

---
+++ src/com/javawork/labthree/Grades.java
@@ -0,0 +1,40 @@
+package JavaClassWork.src.com.javawork.labthree;
+// Kenneth Angelikas
+// 2/2/23
+// Grade Calculator
+// This program accepts your homework and two exam scores as input and computes your grade in the course.
+
+import java.util.Scanner;  // Importing Java Scanner to take user input
+
+public class Grades {
+
+    static int homework() {
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter Homework Weight --> ");
+        int hwWeight = myObj.nextInt();  // Read user input  // hwWeight
+
+        System.out.println("Number of assignments --> ");
+        int hwAssign = myObj.nextInt();  // Read user input  // hwAssign
+
+        System.out.println("Average Homework grade --> ");
+        int averageHw = myObj.nextInt();  // Read user input  // averageHw
+
+        System.out.println("Number of late days used --> ");
+        int ldHw = myObj.nextInt();  // Read user input  // ldHw
+
+        System.out.println("Labs attended --> ");
+        int lattendHw = myObj.nextInt();  // Read user input  // lattendHw
+
+        int[] intArray = new int[]{hwWeight,hwAssign,averageHw,ldHw,lattendHw};
+
+    }
+
+
+    public static void main(String[] args) {  // Main function
+        System.out.println("This program accepts your homework and two exam scores as input and computes your grade in the course.");
+        System.out.println("First we will take your homework grades.");
+        homework();
+
+    }
+}
